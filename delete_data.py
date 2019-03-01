7e9975f7da4e9cd04256721d077a10b016ddce28
Christian Lellmann <christian.lellmann@gmail.com>
2019-03-02 00:22:08 +0100

    Fix bug

--- delete_data.py
+++ delete_data.py
@@ -1,4 +1,5 @@
 import boto3
+import os
 
 dynamodb = boto3.resource('dynamodb', region_name='us-west-1', aws_access_key_id=os.environ['ACCESS_KEY'], aws_secret_access_key=os.environ['SECRET'])
 table = dynamodb.Table('luftdaten')
