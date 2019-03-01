5505fe2675c9dcb94ae5e99be68225421cc69447
Christian Lellmann <christian.lellmann@gmail.com>
2019-03-02 00:14:40 +0100

    Added deletion script

---
+++ delete_data.py
@@ -0,0 +1,13 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-west-1', aws_access_key_id=os.environ['ACCESS_KEY'], aws_secret_access_key=os.environ['SECRET'])
+table = dynamodb.Table('luftdaten')
+scan = table.scan(
+    ProjectionExpression='#k',
+    ExpressionAttributeNames={
+        '#k': 'id'
+    }
+)
+with table.batch_writer() as batch:
+    for each in scan['Items']:
+        batch.delete_item(Key=each)
